32658f911ed6dc5c7356520b7fe21ede4ca74f2c
Joaquin <iglesiasjoaquin201@gmail.com>
2024-07-15 18:04:02 -0300

    arduino

---
+++ arduinoSimple/arduinoSimple.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 0; i<=13; i++)
+  pinMode(i, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i = 0; i <=13; i++){
+    digitalWrite(i, HIGH);
+    delay(100);
+  }
+  for(int i = 0; i <=13; i++){
+    digitalWrite(i, LOW);
+    delay(10);
+
+}}
